71d57db2e44f0e1ab820be7ccaeb8ac135e7c9a7
MariaMCodes <maria.mempin@mq.edu.au>
2023-03-29 18:08:25 +1100

    countdown timer

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ function menuBtnFunction(menuBtn) {
     $(".main-menu").toggleClass("active");
     $(".main-menu a").toggleClass("active");
 }
+
+// Countdown Timer 
+
+
+function updateTimer() {
+    future = Date.parse("apr 21, 2023 24:00:00");
+    now = new Date();
+    diff = future - now;
+
+    days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    hours = Math.floor(diff / (1000 * 60 * 60));
+    mins = Math.floor(diff / (1000 * 60));
+    secs = Math.floor(diff / 1000);
+
+    d = days;
+    h = hours - days * 24;
+    m = mins - hours * 60;
+    s = secs - mins * 60;
+
+    document.getElementById("timer")
+        .innerHTML =
+        '<div>' + d + '<span>days</span></div>' +
+        '<div>' + h + '<span>hours</span></div>' +
+        '<div>' + m + '<span>minutes</span></div>' +
+        '<div>' + s + '<span>seconds</span></div>';
+}
+setInterval('updateTimer()', 1000);
